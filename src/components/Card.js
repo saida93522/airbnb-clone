7c42324299ccc85b1f22157c51d768edfc9b6135
saida93522 <54591143+saida93522@users.noreply.github.com>
2022-05-26 10:28:33 -0500

    feature:displays correct badge text based on value

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,13 @@
 const Card = (props) => {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "Online";
+  }
   return (
     <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
       <img
         src={`../images/${props.img}`}
         className="card--image"
